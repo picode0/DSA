25ffa2f68509206fc37bf28ea4d7ac3e398b6cef
picode0 <164954148+picode0@users.noreply.github.com>
2025-06-30 21:26:18 -0600

    Time: 291 ms (10.88%), Space: 55.2 MB (5.07%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -3,12 +3,14 @@ class Solution {
         //int[] prefix = new int[s.length()];
         Map<Character, List<Integer>> map = new HashMap<>();
         int ret= 0;
+        /*
         List<Integer> first = new ArrayList<>();
         first.add(0);
         map.put(s.charAt(0), first);
+        */
         //prefix[0] = 1;
 
-        for(int i=1;i<s.length();i++){
+        for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
             List<Integer> list = map.getOrDefault(c, new ArrayList<>());
             list.add(i);
