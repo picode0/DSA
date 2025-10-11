deb8046d73fddfa3e556fdeab1297b02635d609c
picode0 <164954148+picode0@users.noreply.github.com>
2025-10-10 23:07:18 -0600

    Time: 3 ms (54.75%), Space: 41.5 MB (98.85%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+
+        map.put(']', '[');
+        map.put(')', '(');
+        map.put('}','{');
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+             if(map.containsKey(c)){
+                if(stack.isEmpty() || stack.pop()!=map.get(c))
+                    return false;
+                
+             }
+             else{
+                stack.push(c);
+             }
+        }
+        return stack.isEmpty();
+    }
+}
