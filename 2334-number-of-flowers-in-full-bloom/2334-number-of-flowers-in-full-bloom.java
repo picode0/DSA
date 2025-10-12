8de668c65a1da097e57550f3896db8a88e9c9054
picode0 <164954148+picode0@users.noreply.github.com>
2025-10-12 00:57:42 -0600

    Time: 385 ms (5.23%), Space: 77.7 MB (48.37%) - LeetHub

---
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+        // 1 3 4 9
+        // 6 7 12 13
+
+        // 1 3
+        // 3 10 
+
+
+        // 3 3 2
+
+        int[] startTime = new int[flowers.length];
+        int[] endTime = new int[flowers.length];
+
+        for(int i=0;i<flowers.length;i++){
+            startTime[i] = flowers[i][0];
+            endTime[i] = flowers[i][1]+1;
+        }
+
+        Arrays.sort(startTime);
+        Arrays.sort(endTime);
+
+        int[] ret = new int[people.length];
+
+        for(int i=0;i<people.length;i++){
+            System.out.println(binaryS(startTime, people[i]) + " " + binaryS(endTime, people[i]));
+            ret[i] = binaryS(startTime,people[i]) - binaryS(endTime,people[i]);
+        }
+        return ret;
+    }
+
+    public int binaryS(int[] times, int target){
+        int left=0, right=times.length;
+
+        while(left<right){
+            int mid = left + (right-left)/2;
+            if(times[mid]>target)
+                right = mid;
+            else
+                left = mid+1; 
+        }
+        return left;
+    }
+}
