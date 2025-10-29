b19dce287929230a91de063eca97f40c16599110
picode0 <164954148+picode0@users.noreply.github.com>
2025-10-29 15:18:34 -0600

    Time: 54 ms (83.03%), Space: 45.6 MB (76.38%) - LeetHub

--- 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.java
+++ 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.java
@@ -1,26 +1,30 @@
 class Solution {
     public int maxDistance(String s, int k) {
         int north = 0, south = 0, west = 0, east = 0;
+        int ver = 0, hor = 0;
         int ret = 0;
-        for(char c: s.toCharArray()){
-            switch(c){
+        for(int i=0;i<s.length();i++){
+            switch(s.charAt(i)){
                 case 'N':
-                    north++;
+                    ver++;
                     break;
                 case 'S':
-                    south++;
+                    ver--;
                     break;
                 case 'W':
-                    west++;
+                    hor++;
                     break;
                 case 'E':
-                    east++;
+                    hor--;
                     break;
             }
+            /*
             int t1 = Math.min(Math.min(north, south), k);
             int t2 = Math.min(Math.min(west, east), k-t1);
 
             ret = Math.max(ret, dirC(north,south,t1) + dirC(west,east, t2));
+            */
+            ret = Math.max(ret, Math.min(Math.abs(ver)+Math.abs(hor)+2*k, i+1));
         }
         return ret; 
     }
