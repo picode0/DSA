27d5d5a7eea25014d55b1dc8d80c28dbaddf29f8
picode0 <164954148+picode0@users.noreply.github.com>
2025-11-10 19:55:23 -0700

    Time: 1 ms (93.07%), Space: 43 MB (6.35%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sidx = 0, tidx = 0;
+        int cnt = 0;
+        if(s.equals(""))
+            return true;
+        for(int i=0;i<t.length();i++){
+            if(t.charAt(i)==s.charAt(sidx)){
+                sidx++;
+                if(++cnt==s.length() || sidx==s.length())
+                    return true;
+            }
+        }
+        return false;
+    }
+}
