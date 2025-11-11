c023d4ee37b922627f285b93dfef4cb135fcdbcd
picode0 <164954148+picode0@users.noreply.github.com>
2025-11-11 14:52:40 -0700

    Time: 4 ms (97.01%), Space: 47.4 MB (11.55%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,5 +1,34 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
+
+        int max = 0;
+        for(int sc: score)
+            max = Math.max(max, sc);
+
+        int[] idx = new int[max+1];
+        Arrays.fill(idx, -1);
+        for(int i=0;i<score.length;i++){
+            idx[score[i]] = i;
+        }
+
+        String[] ret = new String[score.length];
+        String[] medals = new String[]{"Gold Medal", "Silver Medal", "Bronze Medal"};
+        int midx = 0;
+
+        for(int i=idx.length-1;i>=0;i--){
+            if(idx[i]!=-1){
+                if(midx<3){
+                    ret[idx[i]] = medals[midx];
+                }
+                else{
+                    ret[idx[i]] = String.valueOf(midx+1);
+                }
+                midx++;
+            }
+        }
+        return ret;
+
+        /*
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[0]-a[0]);
 
         for(int i=0;i<score.length;i++)
@@ -19,5 +48,6 @@ class Solution {
                 ret[pair[1]] = String.valueOf(i+1);
         }
         return ret;
+        */
     }
 }
