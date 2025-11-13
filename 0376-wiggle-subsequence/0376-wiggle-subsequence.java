fe40bccd07bf772359675cb6e9f8712395e50dd2
picode0 <164954148+picode0@users.noreply.github.com>
2025-11-12 19:16:45 -0700

    Time: 0 ms (100%), Space: 42.4 MB (23.62%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.java
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.java
@@ -1,5 +1,25 @@
 class Solution {
     public int wiggleMaxLength(int[] nums) {
+
+        if(nums.length<2)
+            return 1;
+
+        int prev = nums[1]-nums[0];
+        int ret = prev==0?1:2;
+
+        for(int i=2;i<nums.length;i++){
+            int diff = nums[i] - nums[i-1];
+            if((diff >0 && prev <=0) || (diff<0 && prev >=0)){
+                ret++;
+                prev = diff; 
+            }
+        }
+
+        return ret; 
+
+
+
+        /*
         int[] dp = new int[nums.length];
         if(nums.length==1)
             return 1;
@@ -32,6 +52,7 @@ class Solution {
         }
 
         return dp[dp.length-1];
+        */
 
     }
 }
