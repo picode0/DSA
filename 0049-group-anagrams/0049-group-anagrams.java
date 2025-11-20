933aee241e7eef515c1c0b2d56b2ec621f381fb8
picode0 <164954148+picode0@users.noreply.github.com>
2025-11-20 11:40:43 -0700

    Time: 21 ms (11.78%), Space: 57 MB (5.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,29 +1,25 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         
-        Map<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>(); 
         List<List<String>> ret = new ArrayList<>();
-        for(String str: strs){
-            StringBuilder sb = new StringBuilder();
+        for(String st: strs){
             int[] charcnt = new int[26];
-            for(Character c : str.toCharArray()){
+            StringBuilder sb = new StringBuilder(); 
+            for(char c: st.toCharArray())
                 charcnt[c-'a']++;
-            }
             for(int i=0;i<26;i++){
-                if(charcnt[i]>0){
-                    String temp = i + " " + charcnt[i] + "*";
-                    sb.append(temp);
-                }
+                if(charcnt[i]!=0)
+                    sb.append(i + "*" + charcnt[i]);
             }
-            List<String> list = map.getOrDefault(sb.toString(), new ArrayList<>());
-            list.add(str);
-            map.put(sb.toString(), list);
+            map.putIfAbsent(sb.toString(), new ArrayList<>());
+            map.get(sb.toString()).add(st);
+            //System.out.println(sb.toString());
         }
-
-        for(Map.Entry<String, List<String>> ent: map.entrySet()){
-            ret.add(ent.getValue());
+        for(List<String> list : map.values()){
+            ret.add(new ArrayList<>(list));
         }
-        return ret;
+        return ret; 
 
     }
 }
