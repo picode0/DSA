78db3f3052e4762a47de750bfcbce90feacf5a89
picode0 <164954148+picode0@users.noreply.github.com>
2025-12-15 17:13:56 -0700

    Time: 0 ms (100%), Space: 47.5 MB (55.7%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,5 +1,21 @@
 class Solution {
     public int trap(int[] height) {
+
+        int left = 0, right = height.length-1;
+        int leftmax = 0, rightmax = 0;
+        int ret = 0;
+        while(left<right){
+            if(height[left] <= height[right]){
+                leftmax = Math.max(leftmax, height[left]);
+                ret += leftmax - height[left++];
+            }else{
+                rightmax = Math.max(rightmax, height[right]);
+                ret+= rightmax - height[right--];
+            }
+        }
+
+        return ret;
+        /*
         Stack<Integer> stack = new Stack<>();
         int ret = 0;
         for(int i=0;i<height.length;i++){
@@ -13,6 +29,7 @@ class Solution {
             stack.push(i);
         }
         return ret; 
+        */
         // 0
         // 
     }
