1ee3cd6dd2f9dd41dc2f7233e5e445c7b5a957fb
picode0 <164954148+picode0@users.noreply.github.com>
2026-01-19 20:55:29 -0700

    Time: 2 ms (74.66%), Space: 44.3 MB (30.48%) - LeetHub

---
+++ 3733-minimum-time-to-complete-all-deliveries/3733-minimum-time-to-complete-all-deliveries.java
@@ -0,0 +1,34 @@
+class Solution {
+    public long minimumTime(int[] d, int[] r) {
+        long left = 0, right = (long)(d[0] + d[1])*Math.max(r[0], r[1]);
+
+        while(left<right){
+            long mid = left + (right-left)/2;
+            if(canFinish(d, r, mid)){
+                right = mid;
+            }
+            else
+                left = mid + 1;
+        }
+        return left; 
+    }
+
+    public boolean canFinish(int[] d, int[] r, long time){
+        long t1 = time - time/(long)r[0];
+        long t2 = time - time/(long)r[1];
+
+        long lcm = (long)r[0]*(long)r[1] / gcd((long)r[0], (long)r[1]);
+        long t3 = time - time/lcm;
+
+        return t1>=(long)d[0] && t2>=(long)d[1] && t3>=(long)d[0] + (long)d[1];
+    }
+
+    public long gcd(long a, long b){
+        while(b!=0){
+            long temp = a%b;
+            a=b;
+            b=temp;
+        }
+        return a;
+    }
+}
