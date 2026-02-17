0e47cb5f4e57ba5088c923509172c51556dedf3f
picode0 <164954148+picode0@users.noreply.github.com>
2026-02-16 18:22:49 -0700

    Time: 32 ms (73.97%), Space: 75 MB (24.36%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,10 +1,20 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        // 1 100 150 200 220
-        // 1 2 5 
+
+        int sum = 0;
+        for(int num:nums)
+            sum+=num;
+        
+        if(sum%2==1)
+            return false;
+
+        int target = sum/2;
 
 
+        Boolean[][] memo = new Boolean[nums.length+1][target+1];
 
+        return dfs(nums, nums.length-1, target, memo);
+        /*
         int sum = 0;
         for(int num:nums)
             sum+=num;
@@ -27,6 +37,24 @@ class Solution {
             }
         }
         return dp[nums.length][target];
-
+        */
     }
+
+    public Boolean dfs(int[] nums, int n, int target, Boolean[][] memo){
+        if(target==0)
+            return true;
+        
+        if(n==0 || target <0)
+            return false;
+        
+        if(memo[n][target]!=null)
+            return memo[n][target];
+
+        memo[n][target] = dfs(nums, n-1, target,memo) || dfs(nums, n-1, target-nums[n-1], memo); 
+
+        return memo[n][target];
+        
+    } 
+
+
 }
