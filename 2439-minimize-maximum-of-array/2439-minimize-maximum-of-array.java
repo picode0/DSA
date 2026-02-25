de18276589b24030a6b3c3f558eba9d426784fc6
picode0 <164954148+picode0@users.noreply.github.com>
2026-02-25 03:19:06 -0700

    Time: 15 ms (20.98%), Space: 80.4 MB (64.25%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minimizeArrayValue(int[] nums) {
+        long maxNum = 0;
+
+        for(int num:nums)
+            maxNum = Math.max(maxNum, num);
+
+        long left =0, right = maxNum;
+
+        while(left<right){
+            long mid = left + (right-left)/2;
+
+            if(withinLimit(nums, mid)){
+                right = mid;
+            }
+            else{
+                left = mid +1;
+            }
+        }
+        return (int)left;
+
+    }
+
+    public boolean withinLimit(int[] nums, long limit){
+
+        //int[] temp = nums.clone();
+        long overflow = 0;
+        for(int i=nums.length-1;i>0;i--){
+            long val = nums[i] + overflow;
+            overflow = val>limit?val - limit:0;
+        }
+
+        return nums[0]+overflow<=limit;
+    }
+
+
+}
