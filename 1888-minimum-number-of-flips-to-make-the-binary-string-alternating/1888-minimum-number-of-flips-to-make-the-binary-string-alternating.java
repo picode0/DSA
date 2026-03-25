a070c82712287e16cc7bb89ac4b1e69638722fa6
picode0 <164954148+picode0@users.noreply.github.com>
2026-03-25 02:26:39 -0600

    Time: 21 ms (66.29%), Space: 47.4 MB (42.56%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -2,31 +2,29 @@ class Solution {
     public int minFlips(String s) {
         String newS = s + s;
         int n = s.length();
-        int diff1 =0, diff2 = 0;
+        int diff1 =0;
         int ret = Integer.MAX_VALUE;
         for(int i=0;i<newS.length();i++){
             char c = newS.charAt(i);
 
             char expected1 = i%2==0?'0':'1';
-            char expected2 = i%2==0?'1':'0';
+            //char expected2 = i%2==0?'1':'0';
 
             if(c!=expected1)
                 diff1++;
-            else    
-                diff2++;
-
+            
             if(i>=n){
                 int leftIdx = i-n;
 
                 char oldexpected1 = leftIdx%2==0?'0':'1';
-                char oldexpected2 = leftIdx%2==0?'1':'0';
+                //char oldexpected2 = leftIdx%2==0?'1':'0';
                 if(newS.charAt(leftIdx)!=oldexpected1)
                     diff1--;
-                
-                else
-                    diff2--;
+            
 
             }
+            int diff2 = n - diff1;
+
             if(i>=n-1)
                 ret = Math.min(ret, Math.min(diff1, diff2));
             // 01001001101 01001001101
