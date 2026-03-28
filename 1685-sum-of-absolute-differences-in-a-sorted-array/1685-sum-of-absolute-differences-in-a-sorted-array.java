18e334b412c60d0836789b03df47373dcfb02586
picode0 <164954148+picode0@users.noreply.github.com>
2026-03-28 00:38:58 -0600

    Time: 4 ms (82.13%), Space: 85.3 MB (92.53%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,22 +1,26 @@
 class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
-        int[] prefix = new int[nums.length];
-        prefix[0] = nums[0];
-        for(int i=1;i<nums.length;i++)
-            prefix[i] = prefix[i-1]+nums[i];
+        //int[] prefix = new int[nums.length];
+        //prefix[0] = nums[0];
+
+        int total = 0;
+        for(int num: nums)
+            total+=num;
 
         // 2 3 5
         // 2 5 10
 
-        int[] ret = new int[nums.length]; 
+        int[] ret = new int[nums.length];
+        int leftSum = 0; 
         for(int i=0;i<nums.length;i++){
-            int leftSum = prefix[i] - nums[i];
-            int rightSum = prefix[prefix.length-1] - prefix[i];
+            
+            int rightSum = total - leftSum - nums[i];
+
 
             int leftDiff = nums[i]*i - leftSum;
             int rightDiff = rightSum - nums[i]*(nums.length-i-1);
             ret[i] = leftDiff + rightDiff;
-
+            leftSum+=nums[i];
         }
         return ret;
 
