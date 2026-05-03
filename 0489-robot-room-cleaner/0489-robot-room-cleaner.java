eb76dac12cbcff8b7d2dc8610272bac8bde86581
picode0 <164954148+picode0@users.noreply.github.com>
2026-05-02 23:15:24 -0600

    Time: 4 ms (96.64%), Space: 46.8 MB (45.23%) - LeetHub

---
+++ 0489-robot-room-cleaner/0489-robot-room-cleaner.java
@@ -0,0 +1,56 @@
+/**
+ * // This is the robot's control interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface Robot {
+ *     // Returns true if the cell in front is open and robot moves into the cell.
+ *     // Returns false if the cell in front is blocked and robot stays in the current cell.
+ *     public boolean move();
+ *
+ *     // Robot will stay in the same cell after calling turnLeft/turnRight.
+ *     // Each turn will be 90 degrees.
+ *     public void turnLeft();
+ *     public void turnRight();
+ *
+ *     // Clean the current cell.
+ *     public void clean();
+ * }
+ */
+
+class Solution {
+
+    Set<Pair<Integer,Integer>> visited = new HashSet<>();
+    int[][] dir = new int[][]{{-1, 0},{0 ,1},{1, 0},{0, -1}};
+    Robot robot;
+
+    public void cleanRoom(Robot robot) {
+        this.robot = robot;
+        backtrack(0,0,0);
+    }
+
+    public void backtrack(int row, int col, int d){
+
+        visited.add(new Pair<Integer,Integer>(row, col));
+        robot.clean();
+        for(int i=0;i<4;i++){
+            int newd = (d + i)%4;
+            int newr = row + dir[newd][0];
+            int newc = col + dir[newd][1];
+
+            if(!visited.contains(new Pair<Integer,Integer>(newr, newc)) && robot.move()){
+                backtrack(newr, newc, newd);
+                goBack();
+            }
+            robot.turnRight();
+        }
+
+    }
+
+    public void goBack(){
+        robot.turnRight();
+        robot.turnRight();
+        robot.move();
+        robot.turnRight();
+        robot.turnRight();
+    }
+
+}
