b90ba3d5f71e036cac90a97e5a1c7ce10f7b3c01
picode0 <164954148+picode0@users.noreply.github.com>
2026-05-04 16:56:40 -0600

    Time: 13 ms (80.22%), Space: 114 MB (61.37%) - LeetHub

---
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int largestSubmatrix(int[][] matrix) {
+        int[] dp = new int[matrix[0].length];
+        int ret = 0; 
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                dp[j] = matrix[i][j]==1?dp[j]+1:0;
+            }
+            int[] sorted = dp.clone();
+            Arrays.sort(sorted);
+            for(int k=0;k<sorted.length;k++){
+                int width = sorted.length - k;
+                ret = Math.max(ret, width*sorted[k]);
+            }
+        }
+        return ret;
+    }
+    /*
+    public int maxarea(int[] ones){
+        
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int ret = 0;
+        for(int i=0;i<ones.length;i++){
+            while(stack.peek()!=-1 && ones[stack.peek()]>=ones[i]){
+                int height = ones[stack.pop()];
+                int width = i - stack.peek()-1;
+                ret = Math.max(ret, height*width);
+            }
+            stack.push(i);
+        }
+
+        while(stack.peek()!=-1){
+            int height = ones[stack.pop()];
+            int width = ones.length-stack.peek()-1;
+            ret = Math.max(ret, height*width);
+        }
+        return ret;
+    }
+    */
+
+}
