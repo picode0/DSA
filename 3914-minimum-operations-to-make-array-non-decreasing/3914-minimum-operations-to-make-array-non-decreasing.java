0bf3ba4fe18e183f70bfaf0dc8bbd84622eda7d4
picode0 <164954148+picode0@users.noreply.github.com>
2026-06-18 17:44:23 -0600

    Time: 3 ms (98.67%), Space: 87.6 MB (38.3%) - LeetHub

---
+++ 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long minOperations(int[] nums) {
+        long ret = 0;
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] < nums[i-1])
+                ret+= (nums[i-1]-nums[i]);
+        }
+        return ret;
+    }
+}
