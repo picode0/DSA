d51618d79e692b3ea75b5644fc24b66c48174a8e
picode0 <164954148+picode0@users.noreply.github.com>
2026-06-22 16:09:29 -0600

    Time: 4 ms (98.13%), Space: 74.7 MB (12.61%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
+        /*
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for(int num: nums){ 
@@ -10,5 +11,31 @@ class Solution {
 
 
         return pq.peek();
+        */
+
+        // 0 1 2 3 4 5 
+        //  
+        int maxNum = Integer.MIN_VALUE;
+        int minNum = Integer.MAX_VALUE;
+
+        for(int num: nums){
+            maxNum = Math.max(maxNum, num);
+            minNum = Math.min(minNum, num);
+        }
+
+        int[] countSort = new int[maxNum - minNum +1];
+
+        for(int num: nums){
+            countSort[num-minNum]++;
+        }
+
+        int n = k;
+        for(int i=countSort.length-1;i>=0;i--){
+            n -= countSort[i];
+            if(n<=0)
+                return i+minNum;
+        }
+        return -1;
+
     }
 }
