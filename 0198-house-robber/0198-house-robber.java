2be75ed16e3a24e695075be7451834faa857e605
picode0 <164954148+picode0@users.noreply.github.com>
2026-07-02 20:53:47 -0600

    Time: 0 ms (100%), Space: 42.8 MB (55.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,5 +1,10 @@
 class Solution {
+    int[] memo;
     public int rob(int[] nums) {
+        memo = new int[nums.length];
+        Arrays.fill(memo, -1);
+        return dphelper(nums, 0);
+        /*
         if(nums.length<2)
             return nums[0];
         int[] dp = new int[nums.length];
@@ -12,6 +17,19 @@ class Solution {
             dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
  
         return dp[dp.length-1];
+        */
+    }
 
+    public int dphelper(int[] nums, int idx){
+        if(idx>=nums.length)
+            return 0;
+
+        if(memo[idx]!=-1)
+            return memo[idx];
+
+        int ret = Math.max(dphelper(nums, idx+1), dphelper(nums, idx+2)+nums[idx]);
+        memo[idx] = ret;
+
+        return ret;
     }
 }
