905560a122ea73b1764e777819caee1d683d2a07
picode0 <164954148+picode0@users.noreply.github.com>
2026-07-07 19:11:36 -0600

    Time: 1 ms (100%), Space: 47 MB (55.78%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.java
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.java
@@ -1,8 +1,30 @@
 class Solution {
     public int countMajoritySubarrays(int[] nums, int target) {
-        // 1 2 3 4
+        int n = nums.length;
+        int[] prefix = new int[2*n+1];
+       
+        prefix[n] = 1;
+
+        int cnt = n;
+        int presum = 0;
         int ret = 0;
-        //int cnt = 0; 
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                presum+=prefix[cnt];
+                cnt++;
+                prefix[cnt]++;
+            }
+            else{
+                cnt--;
+                presum-=prefix[cnt];
+                prefix[cnt]++;
+            }
+            ret+=presum;
+        }
+        return ret;
+        /*
+        int ret = 0;
         for(int i=0;i<nums.length;i++){
             int cnt = 0;
             for(int j=i;j<nums.length;j++){
@@ -14,5 +36,6 @@ class Solution {
             }
         }
         return ret; 
+        */
     }
 }
