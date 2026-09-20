f89f76ec88ecfa4974aaab941f72fbd21ece83b5
picode0 <164954148+picode0@users.noreply.github.com>
2026-09-20 15:32:16 -0600

    Time: 3 ms (92.7%), Space: 133.4 MB (19.2%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,21 +1,19 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         
-        int[] maxArr = new int[nums.length];
+ 
         int[] minArr = new int[nums.length];
-        int currmax = Integer.MIN_VALUE;
         int currmin = Integer.MAX_VALUE;
-        for(int i=0;i<maxArr.length;i++){
-            currmax = Math.max(currmax, nums[i]);
-            maxArr[i] = currmax;
-        }
+        int currmax = Integer.MIN_VALUE;
+
         for(int i=minArr.length-1;i>=0;i--){
             currmin = Math.min(currmin, nums[i]);
             minArr[i] = Math.min(currmin, nums[i]);
         }
 
         for(int i=0;i<nums.length;i++){
-            if(maxArr[i] - minArr[i] <= k)
+            currmax = Math.max(currmax, nums[i]);
+            if(currmax - minArr[i] <= k)
                 return i;
         } 
 
